7ec671ac9ceab50767378fd35fa0788983f51a7e
Madu Almeida <dudacsantos450@gmail.com>
2025-07-29 21:25:20 -0300

    Estruturas Condicionais
    Aprendendo a estrutura condicional aninhada(If else if) e a múltipla (Switch case)

---
+++ aula09/ex012.js
@@ -0,0 +1,11 @@
+var agora = new Date()// Cria um objeto Date com a data e hora atuais
+var hora = agora.getHours()// Obtém a hora atual
+console.log(`Agora são exatamente ${hora} horas.`)// Exibe a hora atual
+
+if (hora < 12) {
+    console.log('Bom dia!')
+} else if (hora < 18) {
+    console.log('Boa tarde!')
+}   else {
+    console.log('Boa noite!')
+}
