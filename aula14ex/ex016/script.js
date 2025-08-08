9c5d771bae1084020e556e4d9bcc8484f1f0cc63
Madu Almeida <dudacsantos450@gmail.com>
2025-08-08 19:57:25 -0300

    Exercício 16  - Repetição

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,39 @@
+function contar(){
+    // Recuperando os elementos da página e guardando em variáveis
+    var inicio = document.querySelector('input#txtinicio').value
+    var fim = document.querySelector('input#txtfim').value
+    var passo = document.querySelector('input#txtpasso').value
+    var res = document.querySelector('div#resultado')
+
+    // Verificando se os campos de início, fim e passo estão vazios. O .length retorna o número de caracteres digitados no campo
+    if(inicio.length == 0 || fim.length == 0 || passo.length == 0){
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        // Convertendo os valores digitados em números inteiros
+        inicio = Number(inicio)
+        fim = Number(fim)
+        passo = Number(passo)
+
+        // Verificando se o valor do passo é igual a zero ou negativo
+        if(passo <= 0){
+            window.alert('Passo inválido! Considerando PASSO 1')
+            passo = 1
+        }
+
+        // Iniciando a área de resultado
+        res.innerHTML = 'Contando: '
+
+        // Realizando a contagem crescente
+        if(inicio < fim){
+            for(var contador = inicio; contador <= fim; contador += passo){
+                res.innerHTML += ` ${contador} \u{1F449}` // Adicionando o valor do contador na área de resultado
+            }
+        // Realizando a contagem regressiva
+        } else {
+            for(var contador = inicio; contador >= fim; contador -= passo){
+                res.innerHTML += ` ${contador} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}` // Adicionando a bandeira na área de resultado
+    }
+}
